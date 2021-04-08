8f33fe2fe7a77c753db7bb82e05105fdb90cd0fa
Bernhard Froehler <bernhard@bfroehler.info>
2021-04-08 21:45:21 +0200

    German locale: Remove seconds from date with time format

--- locales/de.php
+++ locales/de.php
@@ -57,6 +57,6 @@ $w2_word_set = array(
 	'Image deleted' => 'Bild gelöscht',
 	'Error deleting image' => 'Fehler beim Löschen des Bildes',
 	// Override TITLE_DATE and TITLE_DATE_NO_TIME if set.
-	'date_format'         => 'd.m.Y H:i:s',
+	'date_format'         => 'd.m.Y H:i',
 	'date_format_no_time' => 'd.m.Y',
 );
